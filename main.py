dd7d0fbb912e9d6df417d743becca3c41628d65f
Christian Emmanuel Yañez Gonzalez <laithg0279@gmail.com>
2021-09-15 15:46:04 -0700

    Buscar Faltante

--- main.py
+++ main.py
@@ -10,10 +10,21 @@ class Main():
         self.Usuario = '18130159' ##Usuario del SISETI
         self.clave = '12345678' ##Clave del SISETI
         self.__Pagina__() #Crea el archivo self.nombre con toda la informacion de la pagina
-        self.__Archivo__() #Carrera matricula correo instrumentos
-    def __Archivo__(self):
+        #self.__Buscar__('21130211') #Recibe una matricula y dice si existe
+        #self.__Mandar__() #Carrera matricula correo instrumentos
+    def __Mandar__(self):
         data = Archivo().Datos(self.name)
         Mensaje(data, self.correo, self.psw)
     def __Pagina__(self):
         Data(self.name, self.Usuario, self.clave)
+    def __Buscar__(self, mat):
+        data = Archivo().Datos(self.name)
+        datos = ""
+        for i in data:
+            if (mat in i["Matricula"]):
+                datos = i
+        if (len(datos) > 0):
+            print(datos)
+        else:
+            print("No esta en la lista de faltantes")
 Main()
